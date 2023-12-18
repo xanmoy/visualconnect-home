3612cf9eb5fcd0a44777630c7a00a86e87b1b1c0
Tanmoy Ganguly <xanmoy@outlook.com>
2023-12-19 02:25:35 +0530

    updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -18,7 +18,7 @@ export const Footer = () => {
                         <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
 
                             <li>
-                                <a href="https://github.com/xanmoy/bytescript-web/blob/main/LICENSE" className="hover:underline me-4 md:me-6">About</a>
+                                <a href="#" className="hover:underline me-4 md:me-6">About</a>
                             </li>
 
                         </ul>
